3ea2a5943fcf6bf7f9213470383f10be31d8da79
Alexandros Metsai <alexmetsai@gmail.com>
2019-05-02 10:35:36 +0300

    Added license and short description

--- image_transformations.py
+++ image_transformations.py
@@ -1,1 +1,13 @@
+# Transform all images/frames inside a specific fodler.
+# These transformations include rotations and flips.
 
+# Copyright (C) 2019 Alexandros I. Metsai
+# alexmetsai@gmail.com
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+
+
+if __name__ == "__main__":
