8fd803eba7cec443d7978bd18caea1cf75bcb740
Alexandros Metsai <alexmetsai@gmail.com>
2019-05-30 21:47:42 +0300

    edit license notice

--- high_pass_filter.py
+++ high_pass_filter.py
@@ -1,21 +1,23 @@
-# Search recursively for all images and
-# pass them through a high pass filter.
+'''
+Search recursively for all images and
+pass them through a high pass filter.
 
-# Copyright (C) 2019 Alexandros I. Metsai
-# alexmetsai@gmail.com
+Copyright (C) 2019 Alexandros I. Metsai
+alexmetsai@gmail.com
 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 from scipy import ndimage
 from scipy.misc import imsave, imread
