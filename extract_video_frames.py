48d6dd894499eb0a7a5f6912326273ee8eb2914d
Alexandros Metsai <alexmetsai@gmail.com>
2020-05-18 15:12:17 +0300

    add main

--- extract_video_frames.py
+++ extract_video_frames.py
@@ -23,26 +23,28 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import cv2, os
 
-# Continue only if user replies "Yes"
-print("\nThis script will create a separate directory for ALL *.mp4"+
-  " files in your working dir!! Are you sure you want to continue? (y/n)")
-x = input()
-if (x!='y' and x!='Y'): exit()
+if __name__ == '__main__':
 
-# Create a separate directory for every video
-# and extract all of its frames inside.
-for file in os.listdir("."):
-    if file.endswith(".mp4"):
-        print("Processing " + file)
-        video_capture = cv2.VideoCapture(file)
-        sucess, image = video_capture.read()
-        folder_name = file.replace(".mp4", "")  # Delete the .mp4 ext
-        os.mkdir(folder_name)
-        count = 1
-        success = True
-        while success:
-            cv2.imwrite(folder_name + "/%d.jpg" % count, image)
-            success, image = video_capture.read()   # Read next frame
-            count+=1
-
-print("Extraction finished")
+    # Continue only if user replies "Yes"
+    print("\nThis script will create a separate directory for ALL *.mp4"+
+      " files in your working dir!! Are you sure you want to continue? (y/n)")
+    x = input()
+    if (x!='y' and x!='Y'): exit()
+    
+    # Create a separate directory for every video
+    # and extract all of its frames inside.
+    for file in os.listdir("."):
+        if file.endswith(".mp4"):
+            print("Processing " + file)
+            video_capture = cv2.VideoCapture(file)
+            sucess, image = video_capture.read()
+            folder_name = file.replace(".mp4", "")  # Delete the .mp4 ext
+            os.mkdir(folder_name)
+            count = 1
+            success = True
+            while success:
+                cv2.imwrite(folder_name + "/%d.jpg" % count, image)
+                success, image = video_capture.read()   # Read next frame
+                count+=1
+    
+    print("Extraction finished")
