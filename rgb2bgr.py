cf7a3d6ad63cbaa0d3283acec0d9e347056bc417
Alexandros Metsai <alexmetsai@gmail.com>
2020-07-03 10:41:10 +0300

    add license -  fix description

--- rgb2bgr.py
+++ rgb2bgr.py
@@ -1,7 +1,23 @@
-# Convert RGB image to BGR.
-# If image is already BGR, it will be converted to RGB.
-# Alexandros I. Metsai
-# alexmetsai@gmail.com
+'''
+Convert all images current working dir to BGR.
+If images are already in BGR, they will be converted to RGB.
+
+Copyright (C) 2019 Alexandros I. Metsai
+alexmetsai@gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 3
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
 
 import numpy as np
 import cv2
