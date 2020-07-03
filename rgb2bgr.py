6894cb3426d683536d0c4a08801d31efc5750cb4
Alexandros Metsai <alexmetsai@gmail.com>
2020-07-03 10:38:59 +0300

    Update rgb2bgr.py

--- rgb2bgr.py
+++ rgb2bgr.py
@@ -1,1 +1,14 @@
+# Convert RGB image to BGR.
+# If image is already BGR, it will be converted to RGB.
+# Alexandros I. Metsai
+# alexmetsai@gmail.com
 
+import numpy as np
+import cv2
+import os
+from scipy import ndimage
+from scipy.misc import imsave, imread
+
+if __name__ == '__main__':
+  # TODO
+  pass
