dda77964d0c47ba1103c17dbc48cab3a1fe21729
alexm <alexmetsai@gmail.com>
2021-10-04 17:05:43 +0300

    minor refactoring and bugfix

--- computer_vision_utils/image_transformations.py
+++ computer_vision_utils/image_transformations.py
@@ -1,8 +1,8 @@
-'''
+"""
 Transform all images/frames inside a specific fodler.
 These transformations include rotations and flips.
 
-Copyright (C) 2019 Alexandros I. Metsai
+Copyright (C) 2019 - 2021 Alexandros I. Metsai
 alexmetsai@gmail.com
 
 This program is free software; you can redistribute it and/or
@@ -17,12 +17,13 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
-'''
+"""
 
 
 import numpy as np
 import cv2
 import os
+
 
 def rotate_90_degrees(folder_name, k=1):
     
@@ -70,7 +71,7 @@ def horizontal_flip(folder_name):
         im = cv2.flip(im, 1)
         
         # replace the original with the fliped image
-        cv2.imwrite(os.path.joing(folder_name, file), im)
+        cv2.imwrite(os.path.join(folder_name, file), im)
         
         print("All images flipped successfully.")
 
@@ -80,7 +81,7 @@ def vertical_flip(folder_name):
     for file in os.listdir(folder_name):
         
         # read image from disc
-        im = cv2.imread(os.path.joing(folder_name, file))
+        im = cv2.imread(os.path.join(folder_name, file))
         
         # flip vertically
         im = cv2.flip(im, 0)
